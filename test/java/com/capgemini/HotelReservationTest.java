06114309238a74f68e1ddb569279d6a2509cfbf7
n-ndini7 <hp@RJs-PC>
2020-10-14 15:17:09 +0530

    [nandini] added UC1 - add hotel

---
+++ test/java/com/capgemini/HotelReservationTest.java
@@ -0,0 +1,17 @@
+package com.capgemini;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HotelReservationTest {
+
+	@Test
+	public void HotelAdded_methodShouldReturnTrue() {
+		HotelReservation service = new HotelReservation();
+		Hotel hotelCheck = new Hotel("Bridgewood", 160);
+		boolean checkIfAdded = service.addHotel(hotelCheck);
+		Assert.assertEquals(checkIfAdded, true);
+	}
+
+	// test for add a hotel
+}
